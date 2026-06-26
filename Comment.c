6c08a27413149632febac03e78b71ef4e305b4a5
Spider4cyber-205 <nafiskamal205@gmail.com>
2026-06-26 23:36:19 +0600

    Practice comments and escape sequence

---
+++ Comment.c
@@ -0,0 +1,13 @@
+// This programme prints my name
+#include <stdio.h>
+int main() 
+{
+      printf("Nafis Kamal\n");
+      printf("United International University \n");
+      printf("01308922468");
+      /* I practice single line comments 
+      multiple line comments 
+      escape sequences in this programme */
+      return 0;
+    
+}
