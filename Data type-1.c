8c74d52590b53040f593e1f104f8c37cb90620be
Spider4cyber-205 <nafiskamal205@gmail.com>
2026-07-08 11:15:14 +0600

    Practice Keyword,Variable,Data type

---
+++ Data type-1.c
@@ -0,0 +1,16 @@
+/*Write programme that prints an integer number, floating number, double number and character*/
+#include <stdio.h>
+int main() 
+{
+    int num1= 10;
+    int num2 = 20;
+    printf("Number is: %d\n", num1);
+    printf("Number is: %d\n", num2);
+    float num3 = 10.5;
+    double num4 = 20.522467;
+    char ch = 'A';
+    printf("Number is: %f\n", num3);
+    printf("Number is: %lf\n", num4);
+    printf("Character is: %c\n", ch);
+    return 0;
+}
