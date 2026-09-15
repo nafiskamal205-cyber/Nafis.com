f49ac62efd586f1363f4cfbf56900451c6cfd6a6
Spider4cyber-205 <nafiskamal205@gmail.com>
2026-09-16 00:56:26 +0600

    Simple Calculator for calculating two numbers using menu

---
+++ Calculator_2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+    int choice;
+    float num1, num2;
+
+    printf("===Menu===\n");
+
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+        case 1:
+            printf("Enter two numbers: ");
+            scanf("%f %f", &num1, &num2);
+            printf("Result: %.2f\n", num1 + num2);
+            break;
+        case 2:
+            printf("Enter two numbers: ");
+            scanf("%f %f", &num1, &num2);
+            printf("Result: %.2f\n", num1 - num2);
+            break;
+        case 3:
+            printf("Enter two numbers: ");
+            scanf("%f %f", &num1, &num2);
+            printf("Result: %.2f\n", num1 * num2);
+            break;
+        case 4:
+            printf("Enter two numbers: ");
+            scanf("%f %f", &num1, &num2);
+            if(num2 != 0)
+                printf("Result: %.2f\n", num1 / num2);
+            else
+                printf("Error! Division by zero.\n");
+            break;
+        default:
+            printf("Invalid choice!\n");
+    }
+
+    return 0;
+
+}
